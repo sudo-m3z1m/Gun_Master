9d7948cded8c490dacc5109e7e9c3f0bd1fddb65
M3Z1M <nizilplay2004@gmail.com>
2023-09-17 21:50:10 +0300

    Well done melee fists. Need to delete animation of hurt_box_area.

--- Weapons/weapon_melee.gd
+++ Weapons/weapon_melee.gd
@@ -2,29 +2,27 @@ extends WEAPON
 
 class_name WEAPON_MELEE
 
+@export var player_animations: Array[String] #Need to delete this
+
+@onready var hurt_box_area: Area2D = $Pivot/area_for_hurt
+
 var target_global_position: Vector2
 
 func attack(_target_global_position: Vector2) -> void:
-	pass
-#	if $Cooldown_Timer.is_stopped() == false:
-#		return
-#	$AnimationPlayer.play(animation)
-##	$Pivot/AnimatedSprite2D.animation = "" Later
-#	$Cooldown_Timer.start(cooldown)
-
-func check_body(body) -> void:
-#	_give_damage(body)
-	pass
+	if _check_cooldown():
+		return
+	cooldown_timer.start(cooldown)
 	
-func check_body_exited(body) -> void:
-#	_give_damage(body)
-	pass
+	player_animation = player_animations[randi() % player_animations.size()]
+	
+	hurt_box_area.body_entered.connect(check_body)
+	anim_player.play(player_animation)
+	anim_sprite.play(attack_animation)
 
-func _give_damage(_body) -> void:
+func check_body(_body) -> void:
+	if _body.get("health_points"):
+		_body.health_points.take_damage(damage)
+	hurt_box_area.body_entered.disconnect(check_body)
+
+func check_body_exited(body) -> void:
 	pass
-#	if $AnimationPlayer.is_playing() == false:
-#		return
-#	var damage_velocity: Vector2 = global_position.direction_to(target_global_position)
-#	for group in ENEMY_GROUP:
-#		if _body.is_in_group(group):
-#			DAMAGE_MANAGER._give_damage(self, _body, damage_velocity, 2)

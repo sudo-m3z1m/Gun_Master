4b8ff2f63d4f249ebea2ba85564e8b37f8ba071e
M3Z1M <nizilplay2004@gmail.com>
2023-09-26 18:20:56 +0300

    Added tween_animations_manager.

---
+++ Prefabs/Game/tween_animations_manager.gd
@@ -0,0 +1,9 @@
+extends Node
+
+class_name TweenAnimations
+
+static func animate_damage_take(target: Object, time: float, start_scale: Vector2) -> void:
+	pass
+
+static func animate_decline(target: Object) -> void:
+	pass
